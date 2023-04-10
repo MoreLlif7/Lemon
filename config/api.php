1012dda63d9f6a957baab73cceb78ce364d396c8
MoreLlif7 <mehdi.raymond1@outlook.fr>
2023-04-11 01:23:47 +0200

    .

--- config/api.php
+++ config/api.php
@@ -1,7 +1,7 @@
 <?php
+$config['API_KEY'] = 'dcd7fd8ed536a4c3d347bf74b1c2361a';
 $config['BASE_URL'] = 'https://api.themoviedb.org/3';
 $config['GENRE'] = "https://api.themoviedb.org/3/genre/movie/list";
-$config['API_KEY'] = 'dcd7fd8ed536a4c3d347bf74b1c2361a';
 $config['SEARCH_FILM'] = "https://api.themoviedb.org/3/search/movie";
 $config['SEARCH_SHOW'] = "https://api.themoviedb.org/3/search/tv";
 $config['SEARCH_ACTEUR'] = "https://api.themoviedb.org/3/search/person";
